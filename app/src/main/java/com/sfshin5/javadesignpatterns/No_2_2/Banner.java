4578277644d8257d9de083c05af59923c190cba3
s-fujiwara <shingo_fujiwara@scsk-hokkaido.co.jp>
2025-12-29 17:13:04 +0900

    No.2 サンプルコード

---
+++ app/src/main/java/com/sfshin5/javadesignpatterns/No_2_2/Banner.java
@@ -0,0 +1,18 @@
+package com.sfshin5.javadesignpatterns.No_2_2;
+
+public class Banner {
+    private String string;
+
+    public Banner(String string) {
+        this.string = string;
+    }
+
+    public void showWithParen() {
+        System.out.println("(" + string + ")");
+    }
+
+    public void showWithAster() {
+        System.out.println("*" + string + "*");
+    }
+}
+
---
+++ app/src/main/java/com/sfshin5/javadesignpatterns/No_2_2/Main.java
@@ -0,0 +1,10 @@
+package com.sfshin5.javadesignpatterns.No_2_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Print p = new PrintBanner("Hello");
+        p.printWeak();
+        p.printStrong();
+    }
+    
+}
