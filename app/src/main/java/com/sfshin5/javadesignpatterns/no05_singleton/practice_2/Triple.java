8ab3e41d7f3f47682f70002ccff1d43a3bfda6f4
s-fujiwara <shingo_fujiwara@scsk-hokkaido.co.jp>
2026-01-07 22:07:18 +0900

    No.5 練習問題2_解答

--- app/src/main/java/com/sfshin5/javadesignpatterns/no05_singleton/practice_2/Triple.java
+++ app/src/main/java/com/sfshin5/javadesignpatterns/no05_singleton/practice_2/Triple.java
@@ -1,22 +1,23 @@
 package com.sfshin5.javadesignpatterns.no05_singleton.practice_2;
 
 public class Triple {
-    private static Triple zero = new Triple(0);
-    private static Triple one = new Triple(1);
-    private static Triple two = new Triple(2);
+    private static Triple[] triple = new Triple[]{
+        new Triple(0),
+        new Triple(1),
+        new Triple(2),
+    };
+    private int id;
 
     private Triple(int id) {
-        System.out.println("インスタンスを生成しました。");
+        System.out.println("インスタンスを生成しました。id：" + id);
+        this.id = id;
     }
     
     public static Triple getInstance(int id) {
-        if (id == 0){
-            return zero;
-        } else if (id == 1){
-            return one;
-        } else if (id == 2){
-            return two;
-        }
-        return null;
+        return triple[id];
+    }
+
+    public String toString(){
+        return "[Triple id=" + id + "]";
     }
 }
